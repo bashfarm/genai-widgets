e8d9b37186d4455ed9356b2205f69e1a1c5c070d
Benjamin Garrard <b2gdevs@gmail.com>
2023-05-19 21:42:31 -0500

    added local login

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Text2SVGPage from './components/text-2-svg-page/text-2-svg-page';
 import AppHeader from './components/app-header/app-header';
 import GeneratedMediaSection from './components/generated-media-section/generated-media-section';
+import LoginPage from './components/login-page/login-page';
 
 
 function App() {
     let [logo, setLogo] = useState<string>('https://res.cloudinary.com/dxqgoyv5b/image/upload/v1676415362/bashful.ai/images/Branding/Logo/logo_1_hc5die.png')
     return (
         <div className='bg-black min-h-screen w-full flex flex-col'>
-            <AppHeader logo_url={logo}/>
-            <Text2SVGPage className='flex-grow'/>
+            {/* <AppHeader logo_url={logo}/>
+            <Text2SVGPage className='flex-grow'/> */}
+            <LoginPage/>
             {/* <GeneratedMediaSection urls={[{url: logo, mediaType: "image"}]}/> */}
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from 'react-auth-kit';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <AuthProvider
+            authType={'cookie'}
+            authName={'_aiart_auth'}
+            cookieDomain={window.location.hostname}
+            cookieSecure={false}
+        >
+            <App />
+        </AuthProvider>
     </React.StrictMode>
 );
 
